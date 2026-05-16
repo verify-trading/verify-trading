0b1ffe15e4b1d561d2dfe8a91762b1e404a489f1
verify-trading <verifytradingadmin@gmail.com>
2026-05-16 06:23:34 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import Script from "next/script";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import { getSiteDescription, getSiteTitle } from "@/lib/site-config";
-
 export const metadata: Metadata = {
   title: getSiteTitle(),
   description: getSiteDescription(),
@@ -12,13 +11,14 @@ export const metadata: Metadata = {
     apple: "/favicon.svg?v=4",
   },
 };
-
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
   interactiveWidget: "resizes-visual",
 };
+
+const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
 export default function RootLayout({
   children,
@@ -41,7 +41,6 @@ export default function RootLayout({
             gtag('config', 'G-QKRRYV1XKF');
           `}
         </Script>
-
         {/* Rewardful tracking */}
         <Script id="rewardful-queue" strategy="afterInteractive">
           {`(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`}
@@ -51,7 +50,34 @@ export default function RootLayout({
           data-rewardful="2f6e2f"
           strategy="afterInteractive"
         />
-
+        {/* Meta Pixel */}
+        {META_PIXEL_ID ? (
+          <>
+            <Script id="meta-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+                alt=""
+              />
+            </noscript>
+          </>
+        ) : null}
         <Providers>{children}</Providers>
       </body>
     </html>
